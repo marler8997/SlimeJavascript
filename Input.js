d5d4a68dc3cd569378d326b04551c025dba91ab1
Jonathan Marler <jonathan.j.marler@hp.com>
2015-07-01 15:51:19 -0600

    Added more menus and AI

--- Input.js
+++ Input.js
@@ -2,6 +2,8 @@ var KEY_A = 65;
 var KEY_D = 68;
 var KEY_W = 87;
 
+var KEY_SPACE = 32;
+
 var KEY_LEFT  = 37;
 var KEY_UP    = 38
 var KEY_RIGHT = 39
@@ -9,8 +11,12 @@ var KEY_RIGHT = 39
 
 var keysDown = {};
 addEventListener("keydown", function(e) {
-    //console.log("keydown '" + e.keyCode + "'");
+  //console.log("keydown '" + e.keyCode + "'");
+  if(e.keyCode == KEY_SPACE) {
+    spaceKeyDown();
+  } else {
     keysDown[e.keyCode] = true;
+  }
 }, false);
 addEventListener("keyup", function(e) {
     //console.log("keyup   '" + e.keyCode + "'");
--- SlimeAI.js
+++ SlimeAI.js
@@ -10,8 +10,10 @@ function countFramesTillBelow(y, vy, limit) {
 }
 
 // Assumes that gameWidth, gameHeight, ball, slimeLeft and slimeRight are globals
-function newSlimeAI(onLeft) {
+function newSlimeAI(onLeft,name) {
   return {
+    name              : name,
+
     onLeft            : onLeft,
 
     meToEnemyWall     : 0, // distance from me to the enemy's wall
@@ -160,7 +162,7 @@ function setPatheticWhiteSlime(ai) {
 	  (this.meToEnemyWall <= 580 && this.ballToEnemyWall < 530 && Math.abs(this.ballToEnemyWall - this.meToEnemyWall) < 100)
 	  ) {
         this.randomJump85Percent();
-      } else if ((Math.pow(this.ballToEnemyWall - this.meToEnemyWall, 2) * 2 + Math.pow(ball.y - me.y, 2) < Math.pow(170, 2)) &&
+      } else if ((Math.pow(this.ballToEnemyWall - this.meToEnemyWall, 2) * 2 + Math.pow(ball.y - me.y, 2) < 28900) &&
                  (this.ballToEnemyWall != this.meToEnemyWall)) {
         this.randomJump85Percent();
       } else if ((Math.pow(this.ballVXToEnemyWall, 2) + Math.pow(ball.velocityY, 2) < 20) &&
@@ -208,3 +210,150 @@ function setPatheticWhiteSlime(ai) {
 
   };
 }
+function setAngryRedSlime(ai) {
+  ai.state = -1;
+
+  ai.randomJump40Percent = function() {
+    if (Math.random() <= 0.40) {
+      this.jump();
+    }
+  };
+
+  ai.moveLogic = function(me,enemy) {
+    keysDown[KEY_UP] = false;
+
+    if(this.ballToEnemyWall < 500)
+      this.state = -1;
+
+    var xWhenBallBelow125 = this.calculateXWhenBallBelow(125);
+    var something;
+    if(me.y > 0 && this.meToEnemyWall < 575) {
+      something = 0;
+    } else {
+      something = 23 + Math.trunc(15 * Math.random());
+    }
+    //console.log('xWhenBallBelow125: ' + xWhenBallBelow125 + ', something: ' + something);
+    if(this.state != -1 || (this.ballVXToEnemyWall == 0 && this.ballToEnemyWall == 800)) {
+      if (this.state == -1) {
+        if(this.enemyToTheirWall > 250) {
+	  this.state = 0;
+	} else if(this.enemyToTheirWall < 200) {
+	  this.state = 1;
+	} else if(this.enemyToTheirWall < 250) {
+	  this.state = 2;
+	}
+	if(Math.random() < 0.35) {
+	  this.state = Math.trunc(3 * Math.random());
+	}
+      }
+      if(this.state == 0) {
+        if(ball.y < 300 && ball.velocityY < -3) {
+          //console.log("moveToWall 1");
+          this.moveToWall();
+          this.jump();
+        }
+      } else if(this.state == 1) {
+        if(ball.y < 300 && ball.velocityY < 0) {
+          //console.log('moveToNet 1');
+          this.moveToNet();
+          this.jump();
+        }
+      } else if(this.state == 2) {
+        if(ball.velocityY > 12 && this.meToEnemyWall < 860) {
+	  this.moveToWall();
+	}
+	if(this.meToEnemyWall >= 860) {
+	  this.stopMovement();
+	}
+	if(ball.velocityY == -3 && this.meToEnemyWall != 800) {
+	  this.jump();
+	}
+	if(ball.velocityY < -12 && me.y != 0 && this.meToEnemyWall >= 845) {
+	  this.moveToNet();
+	}
+      }
+      return;
+    }
+
+    if (xWhenBallBelow125 < 500)
+    {
+      if (Math.abs(this.meToEnemyWall - 800) < 20) {
+        this.stopMovement();
+      } else if (this.meToEnemyWall > 800) {
+        //console.log('moveToNet 2');
+        this.moveToNet();
+      //} else if (this.meToEnemyWall < 800) {
+      } else {
+        //console.log("moveToWall 2");
+        this.moveToWall();
+      }
+      return;
+    }
+    
+    if (Math.abs(this.meToEnemyWall - xWhenBallBelow125) < something)
+    {
+      if (me.y != 0 || Math.random() < 0.3)
+        return;
+
+      if (
+	  (this.meToEnemyWall >= 900 && this.ballToEnemyWall > 830) ||
+	  (this.meToEnemyWall <= 580 && this.ballToEnemyWall < 530 && Math.abs(this.ballToEnemyWall - this.meToEnemyWall) < 100)
+	  ) {
+        this.randomJump40Percent();
+      } else if ((Math.pow(this.ballToEnemyWall - this.meToEnemyWall, 2) * 2 + Math.pow(ball.y - me.y, 2) < 28900) &&
+                 (this.ballToEnemyWall != this.meToEnemyWall)) {
+        this.randomJump40Percent();
+      } else if ((Math.pow(this.ballVXToEnemyWall, 2) + Math.pow(ball.velocityY, 2) < 20) &&
+                 (this.ballToEnemyWall - this.meToEnemyWall < 30) &&
+		 (this.ballToEnemyWall != this.meToEnemyWall)) {
+        this.randomJump40Percent();
+      } else if ((Math.abs(this.ballToEnemyWall - this.meToEnemyWall) < 150) &&
+                 (ball.y > 50) && (ball.y < 400) && (Math.random() < 0.5)) {
+        this.randomJump40Percent();
+      }
+    }
+
+    if (this.state == -1) {
+      if(me.y == 0) {
+        if (Math.abs(this.meToEnemyWall - xWhenBallBelow125) < something) {
+	  this.stopMovement();
+        } else if (xWhenBallBelow125 + something < this.meToEnemyWall) {
+          //console.log('moveToNet 3');
+          this.moveToNet();
+        } else if (xWhenBallBelow125 + something > this.meToEnemyWall) {
+          //console.log("moveToWall 3");
+          this.moveToWall();
+        }
+      } else {
+        if (this.meToEnemyWall < 575) {
+          return;
+        }
+        if (this.meToEnemyWall > 900)
+        {
+          //console.log("moveToWall 4");
+	  this.moveToWall();
+          return;
+        }
+        if (Math.abs(this.meToEnemyWall - this.ballToEnemyWall) < something) {
+	  this.stopMovement();
+        } else if (this.ballToEnemyWall < this.meToEnemyWall) {
+          //console.log('moveToNet 4');
+	  this.moveToNet();
+        } else if (this.ballToEnemyWall > this.meToEnemyWall) {
+          //console.log("moveToWall 5");
+	  this.moveToWall();
+        }
+      }
+    }
+
+  };
+}
+
+var slimeAIs = [
+    {name:"Pathetic White Slime",
+     color:'#fff',
+     initAI:setPatheticWhiteSlime},
+    {name:"Angry Red Slime",
+     color:'#f70',
+     initAI:setAngryRedSlime},
+];
